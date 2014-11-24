736f432f2b077e3f6f9f360268f2e2cc97052b6a
DavidWei <godhuyang@gmail.com>
2014-11-24 11:12:09 +0800

    add test for home page title

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -13,6 +13,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_title("Ruby On Rails Tutorial Sample App | Home")
     end
+    
+    it "should not have a custom title '| Home'" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "| Home")
+    end
+
   end
 
   describe "Help page" do
