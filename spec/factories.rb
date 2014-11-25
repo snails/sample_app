3b44e55c0cf78deff8753d3f925f659ff7e60a48
DavidWei <godhuyang@gmail.com>
2014-11-25 13:08:42 +0800

    Add SSL to production and make users work

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do 
+    name  "David Wei"
+    email "godhuyang@gmail.com"
+    password "helloworld"
+    password_confirmation "helloworld"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,36 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
+  describe "signup functions" do
+
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    it "should not creat new user" do
+      expect{ click_button submit }.not_to change(User, :count)
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "David"
+        fill_in "Email",        with: "godhuyang@gmail.com"
+        fill_in "Password",     with: "12345678"
+        fill_in "Confirmation", with: "12345678"
+      end 
+
+      it "should create a new user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
+  end
 end
